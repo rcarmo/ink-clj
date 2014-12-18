0ac382e403ef6941ec88597899c3c2502cc37a7f
Rui Carmo <rui.carmo@gmail.com>
2014-12-18 17:45:50 +0000

    tests

---
+++ test/ink_clj/test_common.clj
@@ -0,0 +1,7 @@
+(ns ink-clj.test-common
+  (:use [ink-clj.routes :only [app]]
+        [clojure.data.json :only [read-str]]))
+
+(def test-app (app))
+
+(defn read-json [str] (read-str str :key-fn keyword))
