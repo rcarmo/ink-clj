87d9879999aca0ab873db834484281ac1ac2c98e
Rui Carmo <rui.carmo@gmail.com>
2015-02-06 11:13:36 +0000

    added New Relic agent and tracing

--- project.clj
+++ project.clj
@@ -7,8 +7,10 @@
   :aot [ink-clj.main]
   :min-lein-version "2.0.0"
   :uberjar-name "standalone.jar"
+  :java-agents  [[com.newrelic.agent.java/newrelic-agent "3.12.0"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
+                 [com.climate/clj-newrelic "0.2.0"]
                  [compojure "1.3.1"]
                  [environ "1.0.0"]
                  [ring/ring-core "1.3.2"]
--- src/ink_clj/handlers/app.clj
+++ src/ink_clj/handlers/app.clj
@@ -1,7 +1,8 @@
 (ns ink-clj.handlers.app
+  (:use [com.climate.newrelic.trace :only [defn-traced]])
   (:require [ink-clj.tmpls :as tmpl]))
 
-(defn show-landing [req]
+(defn-traced show-landing [req]
   (tmpl/layout {:user-agent (get-in req [:headers "user-agent"])
                  :title "ink-clj"
                  :list ["list item 1"
