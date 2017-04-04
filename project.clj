f1ec3c9f3819430e877b6b7541ce566660bd620d
Rui Carmo <rui.carmo@gmail.com>
2017-04-04 23:32:10 +0100

    updated deps

--- project.clj
+++ project.clj
@@ -7,15 +7,16 @@
   :aot [ink-clj.main]
   :min-lein-version "2.0.0"
   :uberjar-name "standalone.jar"
-  :java-agents  [[com.newrelic.agent.java/newrelic-agent "3.12.0"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.3.1"]
-                 [com.climate/clj-newrelic "0.2.0"]
-                 [compojure "1.3.1"]
-                 [environ "1.0.0"]
-                 [ring/ring-core "1.3.2"]
-                 [org.clojure/data.json "0.2.5"]
-                 [http-kit "2.1.19"]
+  ;; :java-agents  [[com.newrelic.agent.java/newrelic-agent "3.12.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 ;; [com.climate/clj-newrelic "0.2.1"]
+                 [com.microsoft.azure/applicationinsights-web "1.0.7"]
+                 [compojure "1.5.2"]
+                 [environ "1.1.0"]
+                 [ring/ring-core "1.5.1"]
+                 [org.clojure/data.json "0.2.6"]
+                 [http-kit "2.2.0"]
                  ;; [me.raynes/fs "1.4.6"] ;; filesystem walking
                  ;; [enlive "1.1.5"]       ;; HTML parsing
                  ;; [endophile "0.1.2"]    ;; Markdown parsing
@@ -24,8 +25,7 @@
                  ;; [com.taoensso/carmine "1.5.0"]
                  ;; Memoization helpers
                  ;; [org.clojure/core.memoize "0.5.6"]
-
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.2"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  ;; template
                  [me.shenfeng/mustache "1.1"]])
